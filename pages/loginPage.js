a837ff90fbca79f6ab288de9a87e524a291f04a6
Lyubov Menshakova <menshakova222@gmail.com>
2022-11-16 10:18:38 +0300

    Добавлено взаимодействие с API, запрос GET

--- pages/loginPage.js
+++ pages/loginPage.js
@@ -5,16 +5,38 @@ import { Button, TextInput, View } from "react-native";
 //Export default - вызывается при запуске приложения, поэтому здесь обычный экспорт
 
 export function LoginPage({navigation}) {
+
+        const [login, setLogin] = useState("");
+        const [password, setPassword] = useState("");
+
+        const onLoginHandler = (login) => setLogin(login);
+        const onPasswordHandler = (password) => setPassword(password);
+
+
         const onSubmitFormHandler = async () => {
-            navigation.reset({
-                index: 0,
-                routes: [{name: "Профиль пользователя"}]
-        });
+
+            const responceGet = await axios.get('https://test-api.easy4.ru/auth?login=${login}&password=${password}', {})
+            if(responceGet.status = 200){
+
+                alert(responceGet.data.description)
+
+                navigation.reset({
+                
+                    //Резет не позволяет вернуться назад
+                    //Индексы нужны, чтобы вести историю переходов
+                    
+                    index: 0,
+                    routes: [{name: "Профиль пользователя"}]
+            });
+        }
+        else{
+            alert(responceGet.data.description)
+        }
     }
     return(
         <View>
-            <TextInput placeholder="Введите логин..."/>
-            <TextInput placeholder="Введите пароль..."/>
+            <TextInput placeholder="Введите логин..." value={login} onChangeText={onLoginHandler}/>
+            <TextInput placeholder="Введите пароль..." value={password} onChangeText={onPasswordHandler}/>
 
             <Button title="Войти" onPress={onSubmitFormHandler}/>
         </View>
